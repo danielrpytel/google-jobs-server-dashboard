4d4717fff1ec311435ae79ac30f34cf5a87d4bad
Daniel Pytel <danielrpytel@gmail.com>
2023-08-14 17:45:37 -0500

    frontend setup and useAPI get data.

---
+++ front-end/src/hooks/useGetJobPostings.ts
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+export interface Job_Posting_Data {
+	id: number;
+	title: string;
+	company_name: string;
+	location: string;
+	description: string;
+	posting_url: string;
+	inserted_date: Date;
+	boosted: boolean;
+	applied: boolean;
+	flagged: boolean;
+	filtered: boolean;
+	identifier: string;
+}
+
+function useGetJobPostings<Job_Posting_Data>(endpoint: string) {
+	const [data, setData] = useState<Job_Posting_Data[]>([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState<string | null>(null);
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const response = await fetch(endpoint);
+				const responseData = await response.json();
+				setData(responseData);
+			} catch (error) {
+				setError("An error occured while fetching data");
+			} finally {
+				setLoading(false);
+			}
+		}
+
+		fetchData();
+	}, [endpoint]);
+
+	return { data, loading, error };
+}
+
+export default useGetJobPostings;
